598a89007f6b58ac0ac834d90edfcbc514608b13
aakashxok <106732203+aakashxok@users.noreply.github.com>
2022-07-13 13:48:52 +0530

    Update musicals.py

--- FallenRobot/modules/musicals.py
+++ FallenRobot/modules/musicals.py
@@ -1,6 +1,10 @@
 __mod_name__ = "Mᴜsɪᴄ"
 
 __help__ = """
- ❍ /song <song name>*:* Uploads the song in it's best quality available
+ ❍ /song <song name>*:* 𝙐𝙥𝙡𝙤𝙖𝙙𝙨 𝙩𝙝𝙚 𝙨𝙤𝙣𝙜 𝙞𝙣 𝙞𝙩'𝙨 𝙗𝙚𝙨𝙩 𝙦𝙪𝙖𝙡𝙞𝙩𝙮 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚
  💡Ex: `/song Faded Alan Walker`
+𝙄 𝘾𝘼𝙉 𝙋𝙇𝘼𝙔 𝙈𝙐𝙎𝙄𝘾 𝙏𝙊𝙊
+MY MUSIC COMMANDS ??? 
+HERE : https://t.me/WILDBOTUPDATES/7
+
 """
