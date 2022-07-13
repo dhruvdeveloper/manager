f427209806789f6dc1e99a04bada11c9fd9b2f1b
aakashxok <106732203+aakashxok@users.noreply.github.com>
2022-07-13 13:32:01 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -17,14 +17,14 @@ PHOTO = [
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ WILD ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ 𝘼𝘼𝙆𝘼𝙎𝙃 «🇮🇳»](https://t.me/aakashx0202)** \n\n"
-    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
-    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝙄 𝘼𝙈 𝙒𝙄𝙇𝘿 ✘ 𝘽𝙊𝙏​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **𝙈𝙔 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍​ : [ 𝘼𝘼𝙆𝘼𝙎𝙃 «🇮🇳»](https://t.me/aakashx0)** \n\n"
+    TEXT += f"» **𝙇𝙄𝘽𝙍𝘼𝙍𝙔 𝙑𝙀𝙍𝙄𝙎𝙊𝙉 :** `{telever}` \n\n"
+    TEXT += f"» **𝙏𝙀𝙇𝙀𝙏𝙃𝙊𝙉 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 :** `{tlhver}` \n\n"
+    TEXT += f"» **𝙋𝙔𝙍𝙊𝙂𝙍𝘼𝙈 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/wildxrobot?start=help"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/wildxbot?start=help"),
             Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/WildXbotsupport"),
         ]
     ]
