1f421b8986923d8388deee474a5d9d063d659ec6
architxokok <106732203+architxokok@users.noreply.github.com>
2022-08-04 17:49:20 +0530

    Create AboutOwner.py

---
+++ FallenRobot/modules/AboutOwner.py
@@ -0,0 +1,19 @@
+import random
+import asyncio
+from pyrogram import filters
+from FallenRobot import pbot as FallenRobot
+
+ABOUTOWNER_STRINGS = [
+ "MY OWNER IS @aakashok... to know MORE ABOUT HIM CHECK OUT @ABOUT_AAKASH"
+  ]
+@FallenRobot.on_message(filters.command("owner"))
+async def lel(bot, message):
+    ran = random.choice(ABOUTOWNER_STRINGS)
+    await bot.send_chat_action(message.chat.id, "typing")
+    await asyncio.sleep(1.5)
+    return await message.reply_text(text=ran)
+  
+  __mod_name__ = "OWNER"
+  __help__ = """
+ABOUT YOUR FATHER
+❍ /owner *:* ABOUT YOUR FATHER
